6a32ea0a6f39ca8548f35127a2ad127826ff142b
Adriana <s.aguirre.adriana@gmail.com>
2018-12-29 18:16:42 -0600

    Imagen

--- App.js
+++ App.js
@@ -7,7 +7,7 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, Image, View} from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -19,15 +19,19 @@ const instructions = Platform.select({
 type Props = {};
 export default class App extends Component<Props> {
   render() {
+    let pic = {
+      uri: 'http://thesocialsciencepost.com/wp-content/uploads/2016/05/2108594-600x300.jpg'
+    }
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome!</Text>
-        <Text style={styles.instructions}>To get started  !!!!</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Text style={styles.welcome}>Hello World!</Text>
+        <Image source={pic} style={{width: 100, height: 60}}/>
       </View>
     );
   }
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
@@ -35,11 +39,13 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
+    fontSize: 35,
   },
   welcome: {
-    fontSize: 20,
+    fontSize: 40,
     textAlign: 'center',
     margin: 10,
+    color: 'red'
   },
   instructions: {
     textAlign: 'center',
