a1f714f4d6d45c347fbe4da51a23b21e017a6105
Adriana <s.aguirre.adriana@gmail.com>
2018-12-29 18:50:23 -0600

    estilos

--- App.js
+++ App.js
@@ -19,19 +19,29 @@ const instructions = Platform.select({
 type Props = {};
 export default class App extends Component<Props> {
   render() {
-    let pic = {
-      uri: 'http://thesocialsciencepost.com/wp-content/uploads/2016/05/2108594-600x300.jpg'
-    }
+    
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Hello World!</Text>
-        <Image source={pic} style={{width: 100, height: 60}}/>
+        <Imagen  url='http://thesocialsciencepost.com/wp-content/uploads/2016/05/2108594-600x300.jpg'/>
+        <Imagen  url='https://http2.mlstatic.com/disfraz-infantil-super-mario-bros-luigi-peach-cgorra-bigote-D_NQ_NP_823236-MLA26198814006_102017-F.jpg'/>
       </View>
     );
   }
 }
 
-
+class Imagen extends Component{
+  render(){
+    let pic = {
+      uri: this.props.url
+    };
+    return (
+      <View style={styles.image}>
+        <Image source={pic} style={{width: 100, height: 60}}/>
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -51,5 +61,10 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     color: '#333333',
     marginBottom: 5,
+  },
+  image: {
+    marginTop: 30,
+    borderTopColor: 'red',
+    borderTopWidth: 30,
   },
 });
